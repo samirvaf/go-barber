ee72c41964d710d5815a4dc1d93fc9f7a56326bd
Samir Ferreira <svaf2@cin.ufpe.br>
2020-02-19 23:32:39 -0300

    Adding SessionController and Update User routes

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '54c9c190649fe6e06dc01b7fbc3d380a',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import userController from './app/controllers/UserController';
+import sessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+routes.post('/sessions', sessionController.store);
 routes.post('/users', userController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', userController.update);
 
 export default routes;
