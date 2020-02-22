c874bace3218d98f9a9f9e012950f14ec8c679e4
Samir Ferreira <svaf2@cin.ufpe.br>
2020-02-22 14:31:59 -0300

    Finishing upload file backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,23 +2,22 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import userController from './app/controllers/UserController';
-import sessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/sessions', sessionController.store);
-routes.post('/users', userController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', userController.update);
+routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
