54d2339c1f97addbda1cd6ac35753a0dbe26d610
Samir Ferreira <svaf2@cin.ufpe.br>
2020-02-23 18:36:45 -0300

    Adding mailing services

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4763c8bce73e6c',
+    pass: 'ed5b937d603f21',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
